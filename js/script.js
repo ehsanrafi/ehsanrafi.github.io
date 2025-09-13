61a0140ccb323fdc4ab23f66353a5862b40abe64
ehsanrafianjum@gmail.com <ehsan@echo.localdomain>
2025-09-13 21:22:01 +0200

    sections added

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById('current-year').textContent = new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const element = document.querySelector("#years");
+
+    const update = () => element.textContent = ((new Date() - new Date("2004-10-06")) / (1000 * 60 * 60 * 24 * 365.2425)).toFixed(8);
+    
+    update();
+    setInterval(update, 100);
+});
